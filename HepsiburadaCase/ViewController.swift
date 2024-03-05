4f7317339be972e0185851811f874e1ee9c9525e
sercangunonu <sercangunonu@gmail.com>
2024-03-05 09:32:36 +0300

    Initial Commit

---
+++ HepsiburadaCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HepsiburadaCase
+//
+//  Created by Sercan Deniz on 5.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
